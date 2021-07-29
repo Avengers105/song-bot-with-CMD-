00042cefb5db8b782b1a1bd59f691af2c474c112
Avengers105 <85744853+Avengers105@users.noreply.github.com>
2021-07-29 16:53:38 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from pyrogram import Client, filters
+
+import youtube_dl
+from youtube_search import YoutubeSearch
+import requests
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+import os
+from config import Config
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+app = pyrogram.Client(
+      "song",
+       bot_token=Config.BOT_TOKEN,
+       api_id=Config.APP_ID,
+       api_hash=Config.API_HASH,
+       plugins=dict(root="song")
+    )
+app.run()
