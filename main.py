8a603419525798a656f573a6072aaa3e13ef03d6
Avengers105 <85744853+Avengers105@users.noreply.github.com>
2021-07-29 17:00:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 app = pyrogram.Client(
-      "song",
+      "Song",
        bot_token=Config.BOT_TOKEN,
        api_id=Config.APP_ID,
        api_hash=Config.API_HASH,
-       plugins=dict(root="song")
+       plugins=dict(root="Song")
     )
 app.run()
