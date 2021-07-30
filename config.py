ad4c8eb09262c72f7659752e3538752337a635cf
Avengers105 <85744853+Avengers105@users.noreply.github.com>
2021-07-30 13:07:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,4 @@
 import os
-import re
-from youtube_dl import YoutubeDL
-
-class Config:
-    APP_ID = int(os.environ.get("APP_ID", ''))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-    DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
-    msg = {}
+API_ID = int(os.getenv("API_ID", "6"))
+API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
