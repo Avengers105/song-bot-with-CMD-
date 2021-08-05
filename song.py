11e4231675271474a4fde9424fae2f4ec01b6ecf
Avengers105 <85744853+Avengers105@users.noreply.github.com>
2021-08-05 16:06:54 +0530

    Update song.py

--- song.py
+++ song.py
@@ -84,7 +84,7 @@ def a(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎶 <b>Title:</b> <a href="{link}">{title}</a>\n⌚ <b>Duration:</b> <code>{duration}</code>\n📻 <b>Uploaded By:</b> <a href="https://t.me/MusicDownloadv2bot">[MusicDownloadv2bot] Music-Bot</a>'
+        rep = f'🎵 𝐓𝐢𝐭𝐥𝐞 : [{title[:35]}]({link})\n⏰ 𝐃𝐮𝐫𝐚𝐭𝐢𝐨𝐧 : `{duration}`\n👀 𝐕𝐢𝐞𝐰𝐬 : `{views}`\n\n📤 𝐁𝐲 : @Musicdownloadv2bot'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
